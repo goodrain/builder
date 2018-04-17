9248b3c7d35c550b55bb2eef92fb46d2c6592add
zhouyq <zyq916@gmail.com>
2018-04-17 18:22:42 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,13 +2,9 @@
 set -xe
 
 image_name="builder"
-release_type=$1
-release_ver=$2
+release_ver=${1:master}
+docker_tag=${1:latest}
 
-if [ "$release_type" == "" ];then
-  echo "please input release type (community | enterprise | all ) and version"
-  exit 1
-fi
 
 trap 'clean_tmp; exit' QUIT TERM EXIT
 
@@ -19,44 +15,17 @@ function clean_tmp() {
 
 
 function release(){
-  release_name=$1      # master | enterprise | community
-  release_version=${2:-latest}   # latest | 3.2 | 2017.05
   
-  if [ "$release_name" == "master" ];then
-    branch_name=${release_name}
-    git checkout ${branch_name}
-  else
-    branch_name=${release_name}-${release_version}
-    git checkout ${branch_name}
-  fi
-
-  echo "Pull newest code..." && sleep 3
-  git pull
-
   # get commit sha
   git_commit=$(git log -n 1 --pretty --format=%h)
 
 
   # get git describe info
-  release_desc=${release_name}-${release_version}-${git_commit}
+  release_desc=${release_ver}-${git_commit}
 
   sed "s/__RELEASE_DESC__/$release_desc/" Dockerfile >Dockerfile.release
-  docker build -t rainbond/${image_name}:${release_version} -f Dockerfile.release .
-  docker push rainbond/${image_name}:${release_version}
+  docker build -t rainbond/${image_name}:${docker_tag} -f Dockerfile.release .
+  docker push rainbond/${image_name}:${docker_tag}
 }
 
-case $release_type in
-"community")
-    release $1 $release_ver
-    ;;
-"enterprise")
-    release $1 $release_ver
-    ;;
-"master")
-    release $1 $release_ver
-    ;;
-"all")
-    release "community" $release_ver
-    release "enterprise" $release_ver
-    ;;
-esac
+release
