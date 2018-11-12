4b6d83499c881e7269336892786b0612a9f7e059
ysicing zheng <ysbot@ysicing.net>
2018-11-12 11:32:38 +0800

    update v5.0

--- release.sh
+++ release.sh
@@ -1,41 +1,10 @@
 #!/bin/bash
+
 set -xe
 
-image_name="builder"
-release_ver=${1:-v5.0}
-docker_tag=${1:-v5.0}
-
-
-trap 'clean_tmp; exit' QUIT TERM EXIT
-
-function clean_tmp() {
-  echo "clean temporary file..."
-  [ -f Dockerfile.release ] && rm -rf Dockerfile.release
-}
-
-
-function release(){
-  # get commit sha
-  git_commit=$(git log -n 1 --pretty --format=%h)
-  # get git describe info
-  release_desc=${release_ver}-${git_commit}
-
-  sed "s/__RELEASE_DESC__/$release_desc/" Dockerfile >Dockerfile.release
-  docker build --no-cache -t rainbond/${image_name}:${docker_tag} -f Dockerfile.release .
-  docker tag rainbond/${image_name}:${docker_tag}  rainbond/${image_name}
-  if [ -z "$1" ];then
-    docker push rainbond/${image_name}:${docker_tag}
-  else
-    docker tag rainbond/${image_name} goodrain.me/${image_name}
-    docker push goodrain.me/${image_name}
-  fi
-}
-
-case $1 in
-  local)
-      release local
-      ;;
-  *)
-      release
-      ;;
-esac
+commitid=$(git log -n 1 --pretty --format=%h)
+release_ver=$(git branch | grep '^*' | cut -d ' ' -f 2)
+release_desc=${release_ver}-${commitid}
+sed "s/__RELEASE_DESC__/$release_desc/" Dockerfile >Dockerfile.release
+docker build -t rainbond/builder:${release_ver} -f Dockerfile.release .
+docker push rainbond/builder:${release_ver}
