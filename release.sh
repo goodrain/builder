cd29dcc86c0648ed2b8aff08cafa4d5f1a466b2e
barnett <576501057@qq.com>
2018-12-08 16:25:16 +0800

    [REV] change ci build image name

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 set -xe
 
 commitid=$(git log -n 1 --pretty --format=%h)
-release_ver=v5.0
+release_ver=5.0
 release_desc=${release_ver}-${commitid}
 sed "s/__RELEASE_DESC__/$release_desc/" Dockerfile >Dockerfile.release
-docker build -t rainbond/builder:${release_ver} -f Dockerfile.release .
+docker build -t rainbond/rbd-builder:${release_ver} -f Dockerfile.release .
