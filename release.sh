b7456f9d3552ad428d694e3fce897db3e7968be7
barnett <576501057@qq.com>
2019-01-24 09:30:29 +0800

    [REV] change release shell

--- release.sh
+++ release.sh
@@ -3,8 +3,12 @@
 set -xe
 
 commitid=$(git log -n 1 --pretty --format=%h)
-release_ver=5.0
+release_ver=master
 release_desc=${release_ver}-${commitid}
 sed "s/__RELEASE_DESC__/$release_desc/" Dockerfile >Dockerfile.release
 docker build --no-cache -t rainbond/rbd-builder:${release_ver} -f Dockerfile.release .
 rm -rf Dockerfile.release
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then 
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD && \
+  docker push rainbond/rbd-builder:${release_ver};
+fi
