b1651892cfc3691ef8d4f7d6a0e3cc0179914041
ysicing zheng <i@ysicing.me>
2019-02-15 13:39:54 +0800

    update static buildpack

--- release.sh
+++ release.sh
@@ -4,7 +4,25 @@ set -xe
 
 commitid=$(git log -n 1 --pretty --format=%h)
 release_ver=5.0
-release_desc=${release_ver}-${commitid}
-sed "s/__RELEASE_DESC__/$release_desc/" Dockerfile >Dockerfile.release
-docker build --no-cache -t rainbond/rbd-builder:${release_ver} -f Dockerfile.release .
-rm -rf Dockerfile.release
+
+build::local(){
+    release_desc=${release_ver}-${commitid}
+    sed "s/__RELEASE_DESC__/$release_desc/" Dockerfile >Dockerfile.release
+    docker build --no-cache -t goodrain.me/builder -f Dockerfile.release .
+    rm -rf Dockerfile.release
+    [ "$1" == "push" ] && docker push goodrain.me/builder
+}
+
+build::public(){
+    docker tag goodrain.me/builder rainbond/rbd-builder:${release_ver}
+}
+
+case $1 in
+    local)
+        build::local ${@:2}
+    ;;
+    *)
+        build::local
+        build::public
+    ;;
+esac
