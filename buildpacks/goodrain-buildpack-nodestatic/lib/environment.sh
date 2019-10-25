0f68fef53446eb94a8e1a7d9332e953f05846496
guox <guox@goodrain.com>
2019-10-25 10:03:06 +0800

    [Update] nodejs static 源码构建白盒化
    在nodejs static 源码构建功能中：
    - 通过环境变量自定义了yarn install 指定构建私服的功能
    - 通过环境变量自定义了 yarn/npm build 过程中执行的命令
    - 修改一些提示，使构建过程更白盒

--- buildpacks/goodrain-buildpack-nodestatic/lib/environment.sh
+++ buildpacks/goodrain-buildpack-nodestatic/lib/environment.sh
@@ -16,7 +16,7 @@ platform="$os-$cpu"
 export JQ="$BP_DIR/vendor/jq-$os"
 
 create_default_env() {
-  export NPM_CONFIG_LOGLEVEL=${NPM_CONFIG_LOGLEVEL:-error}
+  export NPM_CONFIG_LOGLEVEL=${NPM_CONFIG_LOGLEVEL:-info}
   export NODE_MODULES_CACHE=${NODE_MODULES_CACHE:-true}
   export NODE_ENV=${NODE_ENV:-production}
   export NODE_VERBOSE=${NODE_VERBOSE:-false}
