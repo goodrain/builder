84548d55b03114a3d7671a931d155618d735c1ff
barnettZQG <zengqg@goodrain.com>
2020-12-21 22:40:10 +0800

    change nodejs build log show

--- release.sh
+++ release.sh
@@ -3,12 +3,12 @@
 set -xe
 
 commitid=$(git log -n 1 --pretty --format=%h)
-release_ver=5.3.0
+release_ver=v5.3.0
 
 DOMESTIC_BASE_NAME=${DOMESTIC_BASE_NAME:-'registry.cn-hangzhou.aliyuncs.com'}
 DOMESTIC_NAMESPACE=${DOMESTIC_NAMESPACE:-'goodrain'}
 
-build::local(){
+build::local() {
     release_desc=${release_ver}-${commitid}
     sed "s/__RELEASE_DESC__/$release_desc/" Dockerfile >Dockerfile.release
     docker build --no-cache -t goodrain.me/builder -f Dockerfile.release .
@@ -21,13 +21,12 @@ build::local(){
     fi
 }
 
-build::public(){
+build::public() {
     docker tag goodrain.me/builder rainbond/builder:${release_ver}
-    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then 
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
         docker push rainbond/builder:${release_ver}
-        if [ "${DOMESTIC_BASE_NAME}" ];
-        then
+        if [ "${DOMESTIC_BASE_NAME}" ]; then
             new_tag="${DOMESTIC_BASE_NAME}/${DOMESTIC_NAMESPACE}/builder:${release_ver}"
             docker tag goodrain.me/builder "$new_tag"
             docker login -u "$DOMESTIC_DOCKER_USERNAME" -p "$DOMESTIC_DOCKER_PASSWORD" "${DOMESTIC_BASE_NAME}"
@@ -40,11 +39,11 @@ build::public(){
 }
 
 case $1 in
-    local)
-        build::local ${@:2}
+local)
+    build::local "${@:2}"
     ;;
-    *)
-        build::local
-        build::public
+*)
+    build::local
+    build::public
     ;;
 esac
