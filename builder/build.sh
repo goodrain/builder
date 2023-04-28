7fdd12f8a0a4cd28ac9b321c59aa534e25588cbe
offends <offends4@163.com>
2023-04-28 15:28:41 +0800

    Update java,static buildpack version

--- builder/build.sh
+++ builder/build.sh
@@ -120,7 +120,7 @@ cp -r $app_dir/. $build_root
 export APP_DIR="$app_dir"
 export HOME="$app_dir"
 export REQUEST_ID=$(openssl rand -base64 32)
-export STACK=cedar-14
+export STACK=jammy-20
 export TYPE=${TYPE:-online}
 
 ## Buildpack detection
