289a1d5889ea32285c5f0a0fec6bb78d636d0d28
逆流而上 <1666888816@qq.com>
2023-10-16 11:19:20 +0800

    feat: support-skywalking-agent

--- buildpacks/jvm-common/opt/skywalking.sh
+++ buildpacks/jvm-common/opt/skywalking.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ $ES_ENABLE_SKY == "true" ]];then
-
-    status 'ES_ENABLE_SKY is true'
-#    JMX_EXPORTER_AGENT_VERSION=${JMX_EXPORTER_AGENT_VERSION:-0.15.0}
-#    JMX_EXPORTER_AGENT_PATH=/app/.jmx-exporter
-#    JMX_EXPORTER_HTTP_PORT=${JMX_EXPORTER_HTTP_PORT:-5556}
-#    JMX_EXPORTER_CONFIG=${JMX_EXPORTER_CONFIG:-"/app/.jmx-exporter/config.yaml"}
-#    export JAVA_OPTS="$JAVA_OPTS -javaagent:${JMX_EXPORTER_AGENT_PATH}/jmx_prometheus_javaagent-${JMX_EXPORTER_AGENT_VERSION}.jar=${JMX_EXPORTER_HTTP_PORT}:${JMX_EXPORTER_CONFIG}"
+    export JAVA_OPTS="$JAVA_OPTS -javaagent:/app/.skywalking/skywalking-agent/skywalking-agent.jar -Dskywalking.collector.backend_service=8.130.132.137:11800"
 fi
