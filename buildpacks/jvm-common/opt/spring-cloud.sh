7b56eaa940cb5a8300ea23bc1b20114260b25b5f
逆流而上 <1666888816@qq.com>
2023-10-16 14:23:19 +0800

    feat: support-skywalking-agent

--- buildpacks/jvm-common/opt/spring-cloud.sh
+++ buildpacks/jvm-common/opt/spring-cloud.sh
@@ -3,6 +3,6 @@
 if [[ $ES_ENABLE_SPRING_CLOUD == "true" ]];then
   NACOS_ADDR=${NACOS_ADDR:-nacos.spring-cloud-system.svc.cluster.local:8848}
   curl -X POST "http://$NACOS_ADDR/nacos/v2/console/namespace" -d "namespaceId=${SERVICE_ID}&namespaceName=${SERVICE_ID}"
-
-  export JAVA_OPTS="$JAVA_OPTS -javaagent:/app/.skywalking/skywalking-agent/skywalking-agent.jar -Dskywalking.collector.backend_service=skywalking.spring-cloud-system.svc.cluster.local:12800"
+  curl -o local-spring-cloud.yaml http://www.zhaojiaoben.cn/spring-cloud.yaml
+  export JAVA_OPTS="$JAVA_OPTS -javaagent:/app/.skywalking/skywalking-agent/skywalking-agent.jar -Dskywalking.collector.backend_service=skywalking.spring-cloud-system.svc.cluster.local:12800 -Dspring.config.additional-location=local-spring-cloud.yaml"
 fi
