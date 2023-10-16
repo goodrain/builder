bb2b3cc4df727a1fe6bd44aebeb339c02306e59d
逆流而上 <1666888816@qq.com>
2023-10-16 12:37:47 +0800

    feat: support-skywalking-agent

--- buildpacks/jvm-common/opt/skywalking.sh
+++ buildpacks/jvm-common/opt/skywalking.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ $ES_ENABLE_SKY == "true" ]];then
-    export JAVA_OPTS="$JAVA_OPTS -javaagent:/app/.skywalking/skywalking-agent/skywalking-agent.jar -Dskywalking.collector.backend_service=8.130.132.137:11800"
+if [[ $ES_ENABLE_SPRING_CLOUD == "true" ]];then
+    export JAVA_OPTS="$JAVA_OPTS -javaagent:/app/.skywalking/skywalking-agent/skywalking-agent.jar -Dskywalking.collector.backend_service=skywalking.spring-cloud-system.svc.cluster.local:12800"
 fi
