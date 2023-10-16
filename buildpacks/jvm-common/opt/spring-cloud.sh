693d5d54124599da023e9adff922f88c4637e72d
逆流而上 <1666888816@qq.com>
2023-10-16 14:38:52 +0800

    feat: support-skywalking-agent

--- buildpacks/jvm-common/opt/spring-cloud.sh
+++ buildpacks/jvm-common/opt/spring-cloud.sh
@@ -2,7 +2,15 @@
 
 if [[ $ES_ENABLE_SPRING_CLOUD == "true" ]];then
   NACOS_ADDR=${NACOS_ADDR:-nacos.spring-cloud-system.svc.cluster.local:8848}
-  curl -X POST "http://$NACOS_ADDR/nacos/v2/console/namespace" -d "namespaceId=${SERVICE_ID}&namespaceName=${SERVICE_ID}"
-  curl -o local-spring-cloud.yaml http://www.zhaojiaoben.cn/spring-cloud.yaml
-  export JAVA_OPTS="$JAVA_OPTS -javaagent:/app/.skywalking/skywalking-agent/skywalking-agent.jar -Dskywalking.collector.backend_service=skywalking.spring-cloud-system.svc.cluster.local:12800 -Dspring.config.additional-location=local-spring-cloud.yaml"
+  NS_NAME=${TENANT_ID} # 命名空间名称
+  APP_NAME=${SERVICE_ID} # 应用名称
+  curl -X POST "http://$NACOS_ADDR/nacos/v2/console/namespace" -d "namespaceId=${NS_NAME}&namespaceName=${NS_NAME}"
+  echo "spring:
+    cloud:
+      nacos:
+        discovery:
+          server-addr: ${NACOS_ADDR}
+          namespace: ${NS_NAME}
+          enabled: true" > local-spring-cloud.yaml
+  export JAVA_OPTS="$JAVA_OPTS -javaagent:/app/.skywalking/skywalking-agent/skywalking-agent.jar -Dagent.service_name=${NS_NAME}::${APP_NAME} -Dskywalking.collector.backend_service=skywalking.spring-cloud-system.svc.cluster.local:12800 -Dspring.config.additional-location=local-spring-cloud.yaml"
 fi
