a713acae13be63b572decbdb7fd5d5db707682c9
逆流而上 <1666888816@qq.com>
2023-10-16 15:02:16 +0800

    feat: support-skywalking-agent

--- buildpacks/jvm-common/opt/spring-cloud.sh
+++ buildpacks/jvm-common/opt/spring-cloud.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $ES_ENABLE_SPRING_CLOUD == "true" ]];then
-  SW_AGENT_COLLECTOR_BACKEND_SERVICES=${SW_AGENT_COLLECTOR_BACKEND_SERVICES:-skywalking-oap.spring-cloud-system.svc.cluster.local:11800}
+  export SW_AGENT_COLLECTOR_BACKEND_SERVICES=${SW_AGENT_COLLECTOR_BACKEND_SERVICES:-skywalking-oap.spring-cloud-system.svc.cluster.local:11800}
   NACOS_ADDR=${NACOS_ADDR:-nacos.spring-cloud-system.svc.cluster.local:8848}
   NS_NAME=${TENANT_ID} # 命名空间名称
   APP_NAME=${SERVICE_ID} # 应用名称
