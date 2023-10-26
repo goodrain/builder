3317b753e4dcd910584aa27088bf192db0ee8ce9
逆流而上 <1666888816@qq.com>
2023-10-26 17:37:28 +0800

    feat: support-skywalking-agent

--- buildpacks/jvm-common/opt/spring-cloud.sh
+++ buildpacks/jvm-common/opt/spring-cloud.sh
@@ -3,19 +3,19 @@
 if [[ $ES_ENABLE_SPRING_CLOUD == "true" ]];then
   export SW_AGENT_COLLECTOR_BACKEND_SERVICES=${SW_AGENT_COLLECTOR_BACKEND_SERVICES:-skywalking-oap.spring-cloud-system.svc.cluster.local:11800}
   NACOS_ADDR=${NACOS_ADDR:-nacos.spring-cloud-system.svc.cluster.local:8848}
-  ES_APP_NAME=$(echo "$SERVICE_NAME" | rev | cut -d'-' -f2- | rev) # 团队下的应用英文名
-  ES_SERVICE_NAME=$(echo "$SERVICE_NAME" | rev | cut -d'-' -f1 | rev) # 团队下的应用下的组件英文名
-  curl -X POST "http://$NACOS_ADDR/nacos/v2/console/namespace" -d "namespaceId=${ES_APP_NAME}&namespaceName=${ES_APP_NAME}"
+#  ES_APP_NAME=$(echo "$SERVICE_NAME" | rev | cut -d'-' -f2- | rev) # 团队下的应用英文名
+#  RBD_SERVICE_NAME=$(echo "$SERVICE_NAME" | rev | cut -d'-' -f1 | rev) # 团队下的应用下的组件英文名
+  curl -X POST "http://$NACOS_ADDR/nacos/v2/console/namespace" -d "namespaceId=${RBD_APP_NAME}&namespaceName=${RBD_APP_NAME}"
   echo "spring:
     application:
-      name: ${ES_SERVICE_NAME}
+      name: ${RBD_SERVICE_NAME}
     cloud:
       sentinel:
         datasource:
           rbd_flow:
             nacos:
               serverAddr: ${NACOS_ADDR}
-              dataId: ${ES_SERVICE_NAME}-flow-rules
+              dataId: ${RBD_SERVICE_NAME}-flow-rules
               ruleType: flow
               groupId: SENTINEL_GROUP
               dataType: json
@@ -28,16 +28,16 @@ if [[ $ES_ENABLE_SPRING_CLOUD == "true" ]];then
           refresh-enabled: true
           enabled: true
           server-addr: ${NACOS_ADDR}
-          namespace: ${ES_APP_NAME}
+          namespace: ${RBD_APP_NAME}
           group: DEFAULT_GROUP
           extension-configs:
             - data-id: rbd-gateway-routes.yaml
-              namespace: ${ES_APP_NAME}
+              namespace: ${RBD_APP_NAME}
               group: GATEWAY_GROUP
               refresh: true
         discovery:
           server-addr: ${NACOS_ADDR}
-          namespace: ${ES_APP_NAME}
+          namespace: ${RBD_APP_NAME}
           enabled: true" > local-spring-cloud.yaml
-  export JAVA_OPTS="$JAVA_OPTS -javaagent:/app/.skywalking/skywalking-agent/skywalking-agent.jar -Dskywalking.agent.service_name=${ES_APP_NAME}::${ES_SERVICE_NAME} -Dspring.config.additional-location=local-spring-cloud.yaml"
+  export JAVA_OPTS="$JAVA_OPTS -javaagent:/app/.skywalking/skywalking-agent/skywalking-agent.jar -Dskywalking.agent.service_name=${RBD_APP_NAME}::${RBD_SERVICE_NAME} -Dspring.config.additional-location=local-spring-cloud.yaml"
 fi
