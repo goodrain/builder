99cc9b89d58cff909d8cf558fed96313d9fb55ce
SmallQ <13651281168@163.com>
2024-02-29 17:06:03 +0000

    fix: static packing err and optimize slug size
    Signed-off-by: SmallQ <13651281168@163.com>

--- builder/build.sh
+++ builder/build.sh
@@ -190,9 +190,9 @@ sleep ${PAUSE:-0}
 ## Produce slug
 
 if [[ -f "$build_root/.slugignore" ]]; then
-    tar -z --exclude='.git' -X "$build_root/.slugignore" -C $build_root -cf $slug_file .
+    tar -z --exclude='.git' --exclude='node_modules' -X "$build_root/.slugignore" -C $build_root -cf $slug_file .
 else
-    tar -z --exclude='.git' -C $build_root -cf $slug_file .
+    tar -z --exclude='.git' --exclude='node_modules' -C $build_root -cf $slug_file .
 fi
 
 if [[ "$slug_file" != "-" ]]; then
